b02ba9d2c3cb5cf955152cde9474f28dd5981add
kpue <105126682+kpue@users.noreply.github.com>
2024-10-06 20:02:42 -0500

    first commit

---
+++ articles/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from .models import Article, Comment
+
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
+    list_display = [
+        "title",
+        "body",
+        "author",
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
